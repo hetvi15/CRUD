f3dd6a7ff7c89aaebadef9a1ba9ea371168ae643
hetvi15 <hetvish2000@gmail.com>
2022-01-20 12:41:16 +0530

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+console.log("hrllooo")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors=require('cors')
+const morgan=require('morgan')
+
+const app=express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status',(req,res)=>{
+    res.send({
+        message:"Hello World"
+    })
+})
+app.listen(process.env.PORT || 8082)
+
+/*console.log(express)
+
+console.log("------------------------cls-----------------------------")
+console.log(bodyParser)
+
+console.log("-----------------------------------------------------")
+console.log(cors)
+
+console.log("-----------------------------------------------------")
+console.log(morgan)
+*/
